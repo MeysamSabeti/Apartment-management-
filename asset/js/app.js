8728cfde628f83bc5953e4f3b754294c65b8d661
meysam sabeti <meysam_sabeti8@yahoo.com>
2021-12-06 21:38:53 +0330

    Tab the items and start building the quick access menu

--- asset/js/app.js
+++ asset/js/app.js
@@ -4,5 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems, {   //code materialize
         edge : 'right',    //dar inja mitavan anvae option haro gozasht
         draggable : true   //az site qesmate : sidenav > java script
-    });                                    
+    });      
+    
+    var tabs = document.querySelectorAll('.main_ui_navbar');
+    var instance = M.Tabs.init(tabs, {
+        swipeable:true
+    });
+
+    var actionbutton = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(actionbutton, {});
+
+    
   });
+
+ 
